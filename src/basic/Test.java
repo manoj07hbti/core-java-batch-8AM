06c700ce5c751d7006ad743e944324b6037535f2
60manojkumar@gmail.com <Motivated@2019>
2022-05-06 08:13:30 +0530

    Initial commit

---
+++ src/basic/Test.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // how to print in console
+
+        System.out.println("basic.Test ... this is my first java code");
+
+        System.out.println("basic.Test");
+
+        System.out.println("Java Class");
+
+        System.out.println("basic.Test 123");
+    }
+
+
+}
