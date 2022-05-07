8262e1a9564e0e31e5776b60699ae51d44c9b21e
60manojkumar@gmail.com <Motivated@2019>
2022-05-07 08:29:10 +0530

    GIT Demo

---
+++ src/variables/DemoVariables.java
@@ -0,0 +1,34 @@
+package variables;
+
+public class DemoVariables {
+
+    public static void main(String[] args) {
+
+        // SYNTAX : DATATYPE var_name= value ;
+
+        String student_name= "Rahul" ;
+
+        System.out.println("Printing variables " +student_name);
+
+        String cityName="AGRA";
+
+        System.out.println("City NAME IS "+cityName);
+
+        int age=23;
+
+        System.out.println("Printing age "+age);
+
+        int distance=200;
+
+        System.out.println("Printing distance "+distance);
+        double salary=23456.7;
+        System.out.println("Printing Salary "+salary);
+
+        double price=234.9;
+
+        System.out.println("Price is "+price);
+
+
+
+    }
+}
